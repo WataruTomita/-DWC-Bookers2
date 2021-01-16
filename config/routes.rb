ecbc20571b7f164411f48f98a08a71f25147796b
WataruTomita <tomi.symphonia@gmail.com>
2021-01-16 22:26:01 +0900

    up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'homes#top'
+  # root to: 'homes#top'
+  root 'books#top'
+  get '/about', to: 'books#about'
   devise_for :users
   post 'users/:id' => 'books#create'
   resources :books
