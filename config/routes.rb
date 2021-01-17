cb2aa90def43c2885b66c025e76cbb99f67cd5e6
WataruTomita <tomi.symphonia@gmail.com>
2021-01-17 06:24:32 +0000

    [fixing]about links

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
-  root 'books#top'
-  # get '/about', to: 'books#about'
+  get 'home/about', to: 'homes#about'
   devise_for :users
   post 'users/:id' => 'books#create'
   post 'users' => 'books#create'
   resources :books
-  resources :users
+  resources :users,　only: [:show, :edit, :update]
   # only: [:show, :edit, :update]
 end
